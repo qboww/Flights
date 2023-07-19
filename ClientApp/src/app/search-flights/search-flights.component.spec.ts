a5d493ad1df3c3da98effe06db5ea06a1c038add
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-07-19 19:32:27 +0300

    Add project files.

---
+++ ClientApp/src/app/search-flights/search-flights.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchFlightsComponent } from './search-flights.component';
+
+describe('SearchFlightsComponent', () => {
+  let component: SearchFlightsComponent;
+  let fixture: ComponentFixture<SearchFlightsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchFlightsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SearchFlightsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
