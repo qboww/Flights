9b11afcc87270fabf4d3ce5f663b20a7cf6bb2b1
Evhenii Sarancha <y.i.sarancha@student.khai.edu>
2023-07-20 18:57:39 +0300

    managed flight data as records

---
+++ ReadModels/FlightRm.cs
@@ -0,0 +1,11 @@
+﻿namespace Flights.ReadModels
+{
+    public record FlightRm(
+        Guid Id, 
+        string Airline, 
+        string Price, 
+        TimePlaceRm Departure, 
+        TimePlaceRm Arrival, 
+        int RemainingNumberOfSeats
+        );
+}
